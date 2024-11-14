1b1f13ac546c15554d1a9a36552479469eafcd5e
Soareslili <lidianesantossoares2@gmail.com>
2024-11-14 14:18:25 -0300

    Alterado scripts e css

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-const menu = document.querySelector('.menu');
-const NavMenu = document.querySelector('.Nav-Menu');
 
-menu.addEventListener('click', () => {
-    menu.classList.toggle('ativo');
-    NavMenu.classList.toggle('ativo');
-})
+function toggleMenu() {
+    const navMenu = document.querySelector(".nav-bar"); // Corrigido para usar querySelector e o nome correto da classe
+    // Alterna a exibição do menu
+    if (navMenu.style.display === "block") {
+        navMenu.style.display = "none";
+    } else {
+        navMenu.style.display = "block";
+    }
+}
